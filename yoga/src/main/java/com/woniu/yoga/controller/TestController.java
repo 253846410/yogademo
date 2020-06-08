994f8cb4080aa11f6c3dcce04d7185faac7a1643
253846410 <253846410@qq.com>
2020-06-09 00:20:49 +0800

    Initial commit

---
+++ yoga/src/main/java/com/woniu/yoga/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.woniu.yoga.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author bai
+ * @create 2020-06-09-上午 12:08
+ */
+
+@RestController
+public class TestController {
+    @GetMapping("say")
+    public String say(){
+        System.out.println("say");
+        return "hello";
+    }
+}
