1a782ca6570105c6a34978e36bb628828aec2fb5
253846410 <253846410@qq.com>
2020-06-09 00:25:30 +0800

    init yogademo

--- yoga/src/main/java/com/woniu/yoga/controller/TestController.java
+++ yoga/src/main/java/com/woniu/yoga/controller/TestController.java
@@ -14,6 +14,7 @@ public class TestController {
     @GetMapping("say")
     public String say(){
         System.out.println("say");
+        System.out.println("init");
         return "hello";
     }
 }
